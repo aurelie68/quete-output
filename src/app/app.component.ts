c6ffc172901d22f23c1913d666de99a813bc2765
ZIEGLER Aurelie (SNCF VOYAGEURS / DIRECTION DE LIGNE ALSACE / EQ ESC-VEN MUL) <8301681s@commun.ad.sncf.fr>
2023-06-12 10:34:54 +0200

    exo @Output

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Project';
+  title = 'Output';
+onomatopoeiaList : string[] =["splash","boum","crac","bing"];
+onReceiveNewOnomatopia(event:string):void{
+  this.onomatopoeiaList.push(event);
+}
+
+
 }
---
+++ src/app/create-onomatopia-component/create-onomatopia-component.component.spec.ts
@@ -0,0 +1,21 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { CreateOnomatopiaComponentComponent } from './create-onomatopia-component.component';
+
+describe('CreateOnomatopiaComponentComponent', () => {
+  let component: CreateOnomatopiaComponentComponent;
+  let fixture: ComponentFixture<CreateOnomatopiaComponentComponent>;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [CreateOnomatopiaComponentComponent]
+    });
+    fixture = TestBed.createComponent(CreateOnomatopiaComponentComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
